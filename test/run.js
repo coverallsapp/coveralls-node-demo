ff952e43403f7282922479c1bfb11e127444ae01
Nick Merwin <n@mer.io>
2017-02-22 21:26:37 -0800

    elses

--- test/run.js
+++ test/run.js
@@ -7,9 +7,9 @@ describe("run", function(){
     run.a().should.equal(1);
   });
 
-  // it ("runs method b", function(){
-  //   run.b().should.equal(2);
-  // });
+  it ("runs method b", function(){
+    run.b(1).should.equal(2);
+  });
 
   it ("runs method c", function(){
     // run.c(1).should.equal(1);
