619995d80179dffab3146be0cce0dd53d21fdfd5
Nick Merwin <n@mer.io>
2017-02-22 20:22:25 -0800

    init

---
+++ test/run.js
@@ -0,0 +1,17 @@
+var should = require('should');
+
+var run = require('../index').run;
+
+describe("run", function(){
+  it ("runs method a", function(){
+    run.a().should.equal(1);
+  });
+
+  it ("runs method b", function(){
+    run.b().should.equal(2);
+  });
+
+  it ("runs method c", function(){
+    run.c(1).should.equal(1);
+  });
+});
