3bbf082d10e6c066ba07d99cc391b23bdac8262a
Nick Merwin <n@mer.io>
2017-02-26 12:37:13 -0800

    cov

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # node-demo
+
+```bash
+COVERALLS_REPO_TOKEN=0wCesQawgGWGdAPPL2KzS5lN6JjZSud5q COVERALLS_ENDPOINT=http://localhost:3000 make test-coveralls
+```
--- test/run.js
+++ test/run.js
@@ -8,11 +8,12 @@ describe("run", function(){
   });
 
   it ("runs method b", function(){
+    run.b(0).should.equal(0);
     run.b(1).should.equal(2);
   });
 
   it ("runs method c", function(){
-    // run.c(1).should.equal(1);
+    run.c(1).should.equal(1);
     run.c(0).should.equal(0);
   });
 });
